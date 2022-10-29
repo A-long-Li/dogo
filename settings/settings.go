1625eecf7ddda28a4b249eae1ea1e4197ff5c376
liyajun <332209078@qq.com>
2022-10-29 18:47:53 +0800

    优化配置文件读取

--- settings/settings.go
+++ settings/settings.go
@@ -15,6 +15,38 @@ import (
 	"github.com/spf13/viper"
 )
 
+type AppConfig struct {
+	Name         string `mapstructure:"name"`
+	Mode         string `mapstructure:"mode"`
+	Version      string `mapstructure:"version"`
+	Port         int    `mapstructure:"port"`
+	*LogConfig   `mapstructure:"log"`
+	*MysqlConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    string `mapstructure:"max_size"`
+	MaxAge     string `mapstructure:"max_age"`
+	MaxBackups string `mapstructure:"max_backups"`
+}
+type MysqlConfig struct {
+	Host         string `mapstructure:"host"`
+	User         string `mapstructure:"user"`
+	PassWord     string `mapstructure:"password"`
+	DatabaseName int    `mapstructure:"database_name"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	PassWord string `mapstructure:"password"`
+	Port     int    `mapstructure:"port"`
+	DB       int    `mapstructure:"database_name"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
 func Init() (err error) {
 	viper.SetConfigFile("./settings/config.yaml") // 指定配置文件路径
 	err = viper.ReadInConfig()                    // 查找并读取配置文件
